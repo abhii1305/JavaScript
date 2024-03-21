2c0c8a6184224e28570949eaa61bc28d040f51b2
Abhijeet Singh Yadav <162116952+abhii1305@users.noreply.github.com>
2024-03-21 12:33:56 +0530

    Add files via upload

---
+++ 1_array.js
@@ -0,0 +1,39 @@
+const myArray = [0,1,2,3,4,5]
+const myHeros = ['batman','superman']
+
+const myArray2 = new Array(1,2,3,4)
+// console.log(myArray2[1]);
+
+//Array method
+
+// myArray.push(12)
+// myArray.pop()
+
+// myArray.unshift(7)
+// myArray.shift()
+
+// console.log(myArray.includes(8));
+// console.log(myArray.indexOf(4));
+
+// const newArray = myArray.join(2,2,2,2)
+
+// console.log(myArray);
+
+// console.log(newArray);
+
+//slice and splice
+
+const my1 = myArray.slice(1,4)
+console.log("A",myArray);
+
+console.log(my1);
+console.log("B",myArray);
+
+const my2 = myArray.splice(1,3)
+console.log("c",myArray);
+
+console.log(my2);
+
+
+
+
