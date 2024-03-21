24d1c27d192996a74b1ee8b74b1092ebd334b622
Abhijeet Singh Yadav <162116952+abhii1305@users.noreply.github.com>
2024-03-21 10:14:52 +0530

    Update 6.1_maths.js

--- JavaScript/01-basic/6.1_maths.js
+++ JavaScript/01-basic/6.1_maths.js
@@ -1,13 +1,13 @@
 const score = 100
 const hundred = 10000
 
-// console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6))
-// console.log(Math.ceil(4.6))
-// console.log(Math.floor(4.6))
-// console.log(Math.min(4,6,5,8))
-// console.log(Math.max(4,6,5,8))
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.6))
+console.log(Math.floor(4.6))
+console.log(Math.min(4,6,5,8))
+console.log(Math.max(4,6,5,8))
 
 console.log(Math.random());
 console.log(Math.random()*10);
